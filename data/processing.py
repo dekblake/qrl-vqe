2824c51b1e5e200e153f810488b5b96a8d811a67
dekblake <djuanblake@gmail.com>
2026-03-20 13:20:29 +0000

    update

--- data/processing.py
+++ data/processing.py
@@ -1,5 +1,10 @@
 import yfinance as yf
 import pandas as pd
+import numpy as np
+import datetime as dt 
 
-def training_data(start, end, tickers, time):
+def training_data(start=, end, tickers, interval):
+
+    data = yf.Tickers(tickers).download(interval, start, end)
+    data = pd.dataframe()
     
