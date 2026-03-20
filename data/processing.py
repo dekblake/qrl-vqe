8eaa95ce00eb7b22b2238971a6d8418c9824c984
dekblake <djuanblake@gmail.com>
2026-03-20 14:26:04 +0000

    processing data done

--- data/processing.py
+++ data/processing.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import datetime as dt 
 
-def training_data(tickers, interval):
+def test_train(tickers, interval):
     start = dt.datetime(2020, 1, 1)
     end = dt.datetime(2025, 1, 1)
 
@@ -22,5 +22,20 @@ def training_data(tickers, interval):
 
 if __name__ == "__main__":
 
-    tickers = []
+    assets = [
+    'SPY',   # US Large Cap Equities 
+    'TLT',   # 20+ Year Treasury Bonds 
+    'GLD',   # Gold 
+    'USO',   # Crude Oil
+    'VNQ',   # Real Estate / REITs
+    'EEM',   # Emerging Markets 
+    'UUP',   # US Dollar Index 
+    'XOM',   # Exxon Mobil 
+    'JNJ',   # Johnson & Johnson 
+    'TSLA'   # Tesla 
+]
 
+train_set, test_set = test_train(assets)
+
+train_set.to_csv("data/raw/train_set.csv")
+test_set.to_csv("data/raw/test_set.csv")
