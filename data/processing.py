26b3fa0936b2dae932981aee38bc734ea4d14c30
dekblake <djuanblake@gmail.com>
2026-03-20 14:21:47 +0000

    update

--- data/processing.py
+++ data/processing.py
@@ -3,8 +3,24 @@ import pandas as pd
 import numpy as np
 import datetime as dt 
 
-def training_data(start=, end, tickers, interval):
+def training_data(tickers, interval):
+    start = dt.datetime(2020, 1, 1)
+    end = dt.datetime(2025, 1, 1)
 
-    data = yf.Tickers(tickers).download(interval, start, end)
-    data = pd.dataframe()
+    data = yf.download(tickers, interval, start, end)['Close']
+
+    data = data.ffill().dropna()
+
+    returns = data.pct_change().dropna()
+
+    split = int(len(returns)*.8)
+
+    train_returns = returns.iloc[:split]
+    test_returns = returns.iloc[split:]
     
+    return train_returns, test_returns
+
+if __name__ == "__main__":
+
+    tickers = []
+
